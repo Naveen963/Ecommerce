d1933f1376280b582082b2badc665a3a53201112
Naveen <naveenkumar200101@gmail.com>
2024-04-07 23:31:29 +0530

    First commit

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginPage from '../components/Login/Login.jsx'
+const Login = () => {
+    return (
+        <div>
+            <Login />
+        </div>
+    )
+}
+
+export default LoginPage
